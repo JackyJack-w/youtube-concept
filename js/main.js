c51ac05beebd7ce4d61a5cde73534d1312c370e3
JackyJack-w <82029117+JackyJack-w@users.noreply.github.com>
2021-04-09 19:16:25 +0300

    Добавили правило для адаптива

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 const swiper = new Swiper('.channel-slider', {
     
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints:{
+      1900:{
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300:{
+        slidesPerView: 4
+      },
+      1100:{
+        slidesPerView: 3
+
+      },
+      800:{
+        slidesPerView:2
+      }
+
+
+    },
     
    
   
@@ -17,8 +38,17 @@ const swiper = new Swiper('.channel-slider', {
   const swiper1 = new Swiper('.recommended-slider', {
     
     loop: true,
-    slidesPerView: 2,
-    spaceBetween: 50,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints:{
+
+      1600:{
+        slidesPerView:3
+      },
+      1100:{
+        slidesPerView:2
+      }
+    },
    
   
     // Navigation arrows
@@ -33,8 +63,28 @@ const swiper = new Swiper('.channel-slider', {
   const swiper2 = new Swiper('.recommended-channel-slider', {
     
     loop: true,
-    slidesPerView: 4,
-    
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints:{
+      1900:{
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300:{
+        slidesPerView: 4
+      },
+      1100:{
+        slidesPerView: 3
+
+      },
+      800:{
+        slidesPerView:2
+      }
+
+
+    },
    
   
     // Navigation arrows
@@ -45,3 +95,17 @@ const swiper = new Swiper('.channel-slider', {
   
     
   });
+
+  const searchBtn = document.querySelector('.mobile-search');
+  const mobileSearch = document.querySelector('.input-group')
+  searchBtn.addEventListener('click', () =>{
+    mobileSearch.classList.toggle('is-open');
+
+  });
+
+  if(document.documentElement.scrollWidth <= 640){
+    swiper.destroy();
+    swiper1.destroy();
+    swiper2.destroy();
+    
+  }
